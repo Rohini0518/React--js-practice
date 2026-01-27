2d0bdaa3fdb5e627f783407e831e7f602cb331a7
Rohini0518 <rohinipolina0518@gmail.com>
2026-01-27 17:44:59 +0530

    pratcing usestate with different terms

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+// Vite refresh changes the code; React re-render changes the UI.
+//rerender is not reexecuting
+// main.tsx is executed only once to boot React
+// 👉 useState updates happen inside the already-running React tree
+// 👉 Re-render ≠ re-executing main.tsx
+// after ("root")! ,!-added bcz of typescript,It’s called the non-null assertion operator.
+const container = document.getElementById("root")!;
+export const root = createRoot(container);
+
+export function renderApp() {
+  console.log("main file rendered")
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+renderApp();  // initial render
---
+++ src/react/PrctUseState.jsx
@@ -0,0 +1,74 @@
+import {useState} from 'react';
+
+//   let count = 0;// without storing it on top of function always when tha app render it will become zero only
+
+
+export default function PrctUseState() {
+//   console.log(typeof useState);
+
+  // 1) without using state -have to add render in button for everystate where we need to keep UI Update.
+
+  /* i can see this in console browser-So you see two outputs:
+  1)yours → App.tsx 2)DevTools → installHook.js-It belongs to React DevTools */
+
+  /*  console.log(count);
+  function increment() {
+    console.log("in fn before", count);
+    count++; 
+  renderApp() // this is updating in memory but not updating in UI i.e this component is not rendering after chnage again, no diffing
+    console.log("in fn after", count);
+  } */
+
+  // 2)with useState just primitive data
+  const [counter, setCounter] = useState(0);
+  const [rho, setRho] = useState(55);
+  const [objState,setObjState]=useState({uName:"rho",uNum:2})
+  const [arrState,setArrState]=useState([2,4,5,6]);
+
+  console.log(" very beggining", counter, rho,objState);
+
+  function incrementPrimitiveState() {
+    console.log("before from state  -", counter);
+    setCounter(counter + 1);
+    // react will not rerender now if the any chnage in node then only it will rerender
+    // counter++ // this works same as normal js varubale without using setstate
+    console.log(" from state after state update-", counter);
+  }
+  
+  console.log(" just after inc fn before return", counter);
+ 
+  function objStateFn(){
+    console.log("in fn before setstate",objState)
+    setObjState({...objState,uNum:objState.uNum+2})
+    console.log("in fn after setstate",objState)
+  }
+
+  function arrStateFn(){
+    console.log("arrState before set",arrState)
+    setArrState([...arrState,counter])
+  }
+
+
+  return (
+    <>
+      <h1> React</h1>
+      {/* <p>Count:{count}</p> */}
+      {/* <button onClick={increment}>Increment Count</button> */}
+      <div>
+        <p>Counter:{counter} </p>
+        <button onClick={incrementPrimitiveState}>Increment Count</button>
+        <p>num:{rho} </p>
+        <button onClick={()=>setRho(rho+2)} style={{margin:"10px",backgroundColor:"lightblue"}}>rohini</button>
+      </div>
+
+<div>
+    <p>objNum:{objState.uNum}</p>
+    <button onClick={objStateFn}>objState</button>
+</div>
+<div>
+    <p>objNum:{arrState}</p>
+    <button onClick={arrStateFn}>arrState</button>
+</div>
+    </>
+  );
+}
