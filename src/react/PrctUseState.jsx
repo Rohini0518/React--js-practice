e5ea4c4f8da14c457ba2d0837c4064117e3eb26e
Rohini0518 <rohinipolina0518@gmail.com>
2026-01-28 18:45:47 +0530

    verified how usestate work in async state

--- src/react/PrctUseState.jsx
+++ src/react/PrctUseState.jsx
@@ -1,10 +1,9 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 //   let count = 0;// without storing it on top of function always when tha app render it will become zero only
 
-
 export default function PrctUseState() {
-//   console.log(typeof useState);
+  //   console.log(typeof useState);
 
   // 1) without using state -have to add render in button for everystate where we need to keep UI Update.
 
@@ -22,10 +21,14 @@ export default function PrctUseState() {
   // 2)with useState just primitive data
   const [counter, setCounter] = useState(0);
   const [rho, setRho] = useState(55);
-  const [objState,setObjState]=useState({uName:"rho",uNum:2})
-  const [arrState,setArrState]=useState([2,4,5,6]);
+  const [objState, setObjState] = useState({ uName: "rho", uNum: 2 });
+  const [arrState, setArrState] = useState([2, 4, 5, 6]);
+      console.log("checkin asyncstate")
 
-  console.log(" very beggining", counter, rho,objState);
+  const [checkAsync,setCheckAsync]=useState(44)
+      console.log("checkin asyncstate",checkAsync)
+
+  console.log(" very beggining", counter, rho, objState);
 
   function incrementPrimitiveState() {
     console.log("before from state  -", counter);
@@ -34,21 +37,29 @@ export default function PrctUseState() {
     // counter++ // this works same as normal js varubale without using setstate
     console.log(" from state after state update-", counter);
   }
-  
+
   console.log(" just after inc fn before return", counter);
- 
-  function objStateFn(){
-    console.log("in fn before setstate",objState)
-    setObjState({...objState,uNum:objState.uNum+2})
-    console.log("in fn after setstate",objState)
+
+  function objStateFn() {
+    console.log("in fn before setstate", objState);
+    setObjState({ ...objState, uNum: objState.uNum + 2 });
+    console.log("in fn after setstate", objState);
   }
 
-  function arrStateFn(){
-    console.log("arrState before set",arrState)
-    setArrState([...arrState,counter])
+  function arrStateFn() {
+    console.log("arrState before set", arrState);
+    setArrState([...arrState, counter]);
   }
 
+  function checkasyncState(){
+    console.log("checkin async fn")
+    //initial=44
+  setCheckAsync(checkAsync+1)//45
+  setCheckAsync((p)=>p+5)//44+2=46--
+  setCheckAsync(checkAsync+3)//44+3=47
+  //   setCheckAsync((p)=>p-6)//47-6=38--
 
+  }
   return (
     <>
       <h1> React</h1>
@@ -58,17 +69,26 @@ export default function PrctUseState() {
         <p>Counter:{counter} </p>
         <button onClick={incrementPrimitiveState}>Increment Count</button>
         <p>num:{rho} </p>
-        <button onClick={()=>setRho(rho+2)} style={{margin:"10px",backgroundColor:"lightblue"}}>rohini</button>
+        <button
+          onClick={() => setRho(rho + 2)}
+          style={{ margin: "10px", backgroundColor: "lightblue" }}
+        >
+          rohini
+        </button>
       </div>
 
-<div>
-    <p>objNum:{objState.uNum}</p>
-    <button onClick={objStateFn}>objState</button>
-</div>
-<div>
-    <p>objNum:{arrState}</p>
-    <button onClick={arrStateFn}>arrState</button>
-</div>
+      <div>
+        <p>objNum:{objState.uNum}</p>
+        <button onClick={objStateFn}>objState</button>
+      </div>
+      <div>
+        <p>objNum:{arrState}</p>
+        <button onClick={arrStateFn}>arrState</button>
+      </div>
+      <div>
+         <p>check Async State:{checkAsync}</p>
+        <button onClick={checkasyncState}>checkAsync working of state</button>
+      </div>
     </>
   );
 }
