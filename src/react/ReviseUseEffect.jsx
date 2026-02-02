184f4dbf13af70d87b01d870faf282194bc1c8f8
Rohini0518 <rohinipolina0518@gmail.com>
2026-02-02 15:51:38 +0530

    settimeout,setintervals executed in useeffect and learned how useeffect clear the timer when component render again with state dependency

--- src/react/ReviseUseEffect.jsx
+++ src/react/ReviseUseEffect.jsx
@@ -4,9 +4,9 @@ import "../css/styleCard.css";
 
 export default function ReviseUseEffect() {
   const [data, setData] = useState([]);
-  const [isLoading,setIsLoading]=useState(true)
- const [count,setCount]=useState(8);
- const[age,setAge]=useState(24)
+  const [isLoading, setIsLoading] = useState(true);
+  const [count, setCount] = useState(8);
+  const [age, setAge] = useState(24);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,16 +15,16 @@ export default function ReviseUseEffect() {
           "https://fakestoreapiserver.reactbd.org/api/products?page=1&perPage=10",
         );
         const datajson = await response.json();
-        setIsLoading(false)
+        setIsLoading(false);
         setData(datajson.data);
         console.log(datajson);
       } catch (error) {
         console.error("Error:", error);
       }
-     };
-      console.log("fetch data useeffect")
-          fetchData();
-    }, []);
+    };
+    console.log("fetch data useeffect");
+    fetchData();
+  }, []);
 
   // useEffect(()=>{
   //   console.log("use effect empty ",isLoading)
@@ -35,51 +35,82 @@ export default function ReviseUseEffect() {
   //       console.log(isLoading)
   // },[data])
 
+  // useEffect(() => {
+  //   console.log("age in useeffect ");
+  //   // setCount((p)=>p+0.5)
+  //   //causes infinite loopp
+  // }, [age]);
+
+  // useEffect(() => {
+  //   console.log("count in dependency ,age +1");
+  //   setAge((p) => p + 1);
+  // }, [count]);
+
+
   useEffect(()=>{
-   console.log("age in useeffect ")
-    // setCount((p)=>p+0.5)  
-    //causes infinite loopp 
+const timer=setTimeout(()=>{
+  console.log("settimeout in useeffect c+4,4 sec",count)
+  setCount(count+4)
+},4000)
+ return ()=>clearTimeout(timer)
+  },[])
+
+  useEffect(()=>{
+const timer=setTimeout(()=>{
+  console.log("settimeout in useeffect 2nd time c+2,2 sec",count)
+  setCount(count+2)
+},2000)
+ return ()=>{
+  console.log("cleartimeout")
+ return clearTimeout(timer)}
   },[age])
 
-   useEffect(()=>{
-   console.log("count in dependency ,age +1")
-    setAge((p)=>p+1)
-  },[count])
+//   useEffect(()=>{
+// const timer=setInterval(()=>{
+//   console.log("setInterval in useeffect",age)
+//   setAge((p)=>p+2)
+// },4000)
+//  return ()=>{
+//     console.log("clearInterval in useeeffct")
+//     return clearInterval(timer)}
+//   },[])
+
 
   return (
     <>
       <div className="fashion-wrapper">
-      <div className="fashion-header">
-        <h2 className="title">Fashion Wear</h2>
-        <h4 className="subtitle">sectionSubtitle</h4>
-        <p className="emoji">(❁´◡`❁)</p>
-      </div>
-      <div>
-        <p>count:{count}  </p>
-        <button onClick={()=>setCount(count+1)}>chnageCount</button>
-      </div>
-      <div>
-        <p>age:{age}  </p>
-        <button onClick={()=>setAge(age-2)}>chnageAge</button>
-      </div>
-    {isLoading ?<div>Loading----</div>:
-      <div className="product-grid">
-        {data.map((product) => (
-          <div className="product-card" key={product.title}>
-            <img
-              src={product.image}
-              alt={product.title}
-              className="product-img"
-            />
-            <p className="product-title">{product.title}</p>
-            <p className="product-price">Price:$ {product.price}</p>
-            <p >Ratings:{product.rating}</p>
-
+        <div className="fashion-header">
+          <h2 className="title">Fashion Wear</h2>
+          <h4 className="subtitle">sectionSubtitle</h4>
+          <p className="emoji">(❁´◡`❁)</p>
+        </div>
+        <div>
+          <p>count:{count} </p>
+          <button onClick={() => setCount(count + 1)}>chnageCount</button>
+        </div>
+        <div>
+          <p>age:{age} </p>
+          <button onClick={() => setAge(age - 2)}>chnageAge</button>
+        </div>
+        {isLoading ? (
+          <div>Loading----</div>
+        ) : (
+          <div className="product-grid">
+            {data.map((product) => (
+              <div className="product-card" key={product.title}>
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="product-img"
+                />
+                <p className="product-title">{product.title}</p>
+                <p className="product-price">Price:$ {product.price}</p>
+                <p>Ratings:{product.rating}</p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>}
-
-    </div>
+        )}
+      </div>
     </>
   );
 }
