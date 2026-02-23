716edfc5dd6103ca9a72051832c64c243e6365fe
Rohini0518 <rohinipolina0518@gmail.com>
2026-02-23 17:44:34 +0530

    app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import AgeCalculator from "./react/AgeCalculator";
+import FormSubmit from "./react/FormSubmit";
 
 function App() {
+ console.log("app -file run")
   return (
     <>
       <h1>Vite + React</h1>
+  <FormSubmit/>
+  <AgeCalculator/>
     </>
   );
 }
