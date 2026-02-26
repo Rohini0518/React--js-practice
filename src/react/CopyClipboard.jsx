f14328cc93ce47f0b59775a8e9121da136318963
Rohini0518 <rohinipolina0518@gmail.com>
2026-02-26 17:23:05 +0530

    copy to clipboard react que

---
+++ src/react/CopyClipboard.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function CopyClipboard() {
+
+  
+
+  return (
+    <div className="copyToClipboard">
+      <h1>Copy to Clipboard</h1>
+      <p>Click the button to copy the text</p>
+
+      <div className="copyToClipboard-container">
+        <div className="form">
+          <label htmlFor="text">
+            Enter your text:
+            <input
+              type="text"
+              id="text"
+              data-testid="input-field"
+              placeholder="Type Something"
+            />
+          </label>
+          <button
+            onClick={() => {
+              handleCopy(text);
+            }}
+            className="btn"
+            data-testid="copy-button"
+          >
+            Copy
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CopyClipboard;
